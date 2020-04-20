287351d98d30d06673b62a1fec384002c4747b51
LI ZHENG <zpc@deakin.edu.au>
2020-04-21 03:01:37 +1000

    Initial Commit

---
+++ Assets/Gamemanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    private string sceneName;
+    public void GoSetting(){
+        SceneManager.LoadScene("SettingScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoStart(){
+        SceneManager.LoadScene("NewgameScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoLoad(){
+        SceneManager.LoadScene("OldgameScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoCancel(){
+        SceneManager.LoadScene("Cancel"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+}
