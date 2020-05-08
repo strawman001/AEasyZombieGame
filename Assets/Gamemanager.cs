210eab508f007b6cd33ca45b4729025a670d060d
LI ZHENG <zpc@deakin.edu.au>
2020-05-09 02:45:16 +1000

    Complete many game UI

--- Assets/Gamemanager.cs
+++ Assets/Gamemanager.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
+    private int BrainNumber;
     private string sceneName;
     public void GoSetting(){
         SceneManager.LoadScene("SettingScene"); 
         sceneName = SceneManager.GetActiveScene().name;
     }
     public void GoStart(){
-        SceneManager.LoadScene("NewgameScene"); 
+        SceneManager.LoadScene("FirstGameScene");
+       // int BrainNumber = 0;
         sceneName = SceneManager.GetActiveScene().name;
     }
-    public void GoLoad(){
-        SceneManager.LoadScene("OldgameScene"); 
+    public void GoMain(){
+        SceneManager.LoadScene("MainScene"); 
         sceneName = SceneManager.GetActiveScene().name;
     }
-    public void GoCancel(){
-        SceneManager.LoadScene("Cancel"); 
+    public void GoInform()
+    {
+        SceneManager.LoadScene("InformationScene");
         sceneName = SceneManager.GetActiveScene().name;
     }
 }
