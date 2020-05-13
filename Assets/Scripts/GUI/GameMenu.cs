836b151a42424f243ac51b643bfaee7d399e33c0
LI ZHENG <zpc@deakin.edu.au>
2020-05-14 03:03:44 +1000

    complete the select level scene and slide function

--- Assets/Scripts/GUI/GameMenu.cs
+++ Assets/Scripts/GUI/GameMenu.cs
@@ -7,8 +7,6 @@ public class GameMenu : MonoBehaviour
 {
     // Start is called before the first frame update
   //  public Image gameMenuImage;
-
- //   public Toggle BGMToggle;
  //   private AudioSource BGMSource;//MARKER BGM Source
  private string sceneName;
  public GameObject gameMenuImage;
@@ -28,52 +26,9 @@ public class GameMenu : MonoBehaviour
      SceneManager.LoadScene("MainScene"); 
      sceneName = SceneManager.GetActiveScene().name;
  }
-    
-    /*
-    public void Resume()
-    {
-        Debug.Log("RESUME");
-        gameMenuImage.gameObject.SetActive(false);
-        Time.timeScale = 1;
-        GameManager.instance.isPaused = false;
-    }
-    public void Pause()
-    {
-        Debug.Log("PAUSE");
-        gameMenuImage.gameObject.SetActive(true);
-        Time.timeScale = 0;
-        GameManager.instance.isPaused = true;
-    }
-    public void BGMToggleButton()
-    {
-        if(BGMToggle.isOn)
-        {
-            //OPEN the BGM
-            PlayerPrefs.SetInt("BGM", 1);//OPEN
-            Debug.Log(PlayerPrefs.GetInt("BGM"));
-        }
-        else
-        {
-            //CLOSE the BGM
-            PlayerPrefs.SetInt("BGM", 0);//CLOSE
-            Debug.Log(PlayerPrefs.GetInt("BGM"));
-        }
-    }
-    private void BGMManager()
-    {
-        if (PlayerPrefs.GetInt("BGM") == 1)
-        {
-            BGMToggle.isOn = true;
-            BGMSource.enabled = true;
-        }
-        else if (PlayerPrefs.GetInt("BGM") == 0)
-        {
-            BGMToggle.isOn = false;
-            BGMSource.enabled = false;
-        }
-    }*/
+ 
     void Update()
     {
-        
+      
     }
 }
---
+++ Assets/Scripts/GUI/Gamemanager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    private int BrainNumber;
+    private string sceneName;
+    public void GoSetting(){
+        SceneManager.LoadScene("SettingScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void GoSelect()
+    {
+        SceneManager.LoadScene("SelectScene");
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoFirstLevel(){
+        SceneManager.LoadScene("FirstGameScene");
+       // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoSecondLevel(){
+        SceneManager.LoadScene("SecondGameScene");
+        // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoThirdGame(){
+        SceneManager.LoadScene("ThirdGameScene");
+        // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoMain(){
+        SceneManager.LoadScene("MainScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+}
