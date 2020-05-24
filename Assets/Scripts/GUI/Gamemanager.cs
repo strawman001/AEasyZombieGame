bdc2f3f9638d49b254a9b7d02b2b83978d020e55
LI ZHENG <zpc@deakin.edu.au>
2020-05-24 23:43:49 +1000

    no message

---
+++ Assets/Scripts/GUI/Gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    private int BrainNumber;
+    private string sceneName;
+    public void GoSelect()
+    {
+        SceneManager.LoadScene("SelectScene");
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoFirstLevel(){
+        SceneManager.LoadScene("FirstGameScene");
+       // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoSecondLevel(){
+        SceneManager.LoadScene("SecondGameScene");
+        // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoThirdLevel(){
+        SceneManager.LoadScene("ThirdGameScene");
+        // int BrainNumber = 0;
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    public void GoMain(){
+        SceneManager.LoadScene("MainScene"); 
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+}
