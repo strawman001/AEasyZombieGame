530d9a524b985c595dd68fd684eea21bb55386cc
LI ZHENG <zpc@deakin.edu.au>
2020-05-24 23:22:51 +1000

    complete many functions, add models, the animation play functions

---
+++ Assets/Scripts/Action/camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 offsetStation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.LookAt(target.position);
+        offsetStation = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = offsetStation + target.position;
+
+    }
+}
