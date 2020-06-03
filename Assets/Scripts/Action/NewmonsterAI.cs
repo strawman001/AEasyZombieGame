214730b63a621259bb4ee7424e5166e2f9dbf4bb
LI ZHENG <zpc@deakin.edu.au>
2020-06-03 13:22:19 +1000

    Cooperate wil YUHAO to add the terrain and updape game scene

--- Assets/Scripts/Action/NewmonsterAI.cs
+++ Assets/Scripts/Action/NewmonsterAI.cs
@@ -4,150 +4,67 @@ using UnityEngine;
 
 public class NewmonsterAI : MonoBehaviour
 {
-    public const int AI_Null = 10;
-    public const int AI_Idle = 0;
-    public const int AI_Patrol = 1;
-    public const int AI_Pursuit = 2;
-    public const int AI_Attack = 3;
-    public const int AI_Attack_Idle = 4;
-    public const int AIA_BackPoint = 5;
+    public const int Null = 2;
+
 
     public const float followUpDistance = 3.0f;
     public const float attackDistance = 2.0f;
 
-    public const float vAttackDeltaTime = 1.0f;
-
     public const float maxFollow = 20.0f;
-    public const float speed = 2.0f;
-    public const float runSpeed = 3.0f;
-    public const float vEscapeSpeed = 6.0f;
+    public const float speed = 5.0f;
+
 
     public Rect vRect = new Rect(0, 0, 10, 10);
 
     public Vector2 Dis;
     public Vector2 srcPoint;
 
-    public GameObject player;
-    public int vStartState;
-    public int vLostState;
+ 
+    public int StartState;
+    public int LostState;
     // Use this for initialization
     void Start()
     {
-        vStartState = AI_Idle;
-        vLostState = AI_Null;
+        StartState = 0;
+        LostState = Null;
         srcPoint = new Vector2(transform.position.x, transform.position.z);
-        player = GameObject.Find("Viking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (vStartState)
+        switch (StartState)
         {
-            case AI_Idle:
-                if (vStartState != vLostState)
+            case 0:
+                if (StartState != LostState)
                 {
-                    vLostState = vStartState;
-                    Debug.Log("1");
+                    LostState = StartState;
                 }
-                fGetPatrolPosition();
-                vStartState = AI_Patrol;
+                GetPatrolPosition();
+                StartState = 1;
                 break;
-            case AI_Patrol:
-                if (vStartState != vLostState)
+            case 1:
+                if (StartState != LostState)
                 {
-                    vLostState = vStartState;
-                    Debug.Log("2");
+                    LostState = StartState;
                 }
-                fPatrovRect();
+                PatrovRect();
                 if (Vector2.Distance(Dis, new Vector2(transform.position.x, transform.position.z)) < 0.4f)
-                {
-            
-
-                    vStartState = AI_Idle;
-                }
-                if (Vector3.Distance(transform.position, player.transform.position) < followUpDistance)
-                {
-                    vStartState = AI_Pursuit;
-                }
-
-                break;
-            case AI_Pursuit:
-                if (vStartState != vLostState)
-                {
-                    vLostState = vStartState;
-                    Debug.Log("3");
-                }
-                fPursuitHero();
-                if (Vector3.Distance(new Vector3(srcPoint.x, transform.position.y, srcPoint.y), transform.position) > maxFollow && Vector3.Distance(transform.position, player.transform.position) > followUpDistance)
-                {
-                    vStartState = AIA_BackPoint;
-                }
-                if (Vector3.Distance(transform.position, player.transform.position) < attackDistance)
-                {
-                    vStartState = AI_Attack;
-                }
-                break;
-            case AI_Attack:
-                if (vStartState != vLostState)
-                {
-                    vLostState = vStartState;
-                    Debug.Log("4");
-                }
-                fAttackHero();
-                if (Time.time - backUptime >= 0.2f)
-                {
-                    backUptime = Time.time;
-                    vStartState = AI_Attack_Idle;
-                }
-                break;
-            case AI_Attack_Idle:
-                if (vStartState != vLostState)
-                {
-                    vLostState = vStartState;
-                }
-                vStartState = AI_Pursuit;
-                break;
-            case AIA_BackPoint:
-                if (vStartState != vLostState)
-                {
-                    vLostState = vStartState;
-                }
-                fBackPoint();
-                if (Vector2.Distance(srcPoint, new Vector2(transform.position.x, transform.position.z)) < 1)
-                {
-                    vStartState = AI_Idle;
+                {       
+                    StartState = 0;
                 }
                 break;
         }
 
     }
 
-    private void fBackPoint()
-    {
-        transform.LookAt(new Vector3(srcPoint.x, transform.position.y, srcPoint.y));
-        transform.Translate(Vector3.forward * vEscapeSpeed * Time.deltaTime);
-    }
-    public float backUptime;
-    private void fAttackHero()
-    {
-
-    }
-
-    private void fPursuitHero()
-    {
-        transform.LookAt(player.transform.position);
-        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.forward * vEscapeSpeed * Time.deltaTime);
-    }
-
-    private void fPatrovRect()
+    private void PatrovRect()
     {
         transform.LookAt(new Vector3(Dis.x, transform.position.y, Dis.y));
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    private void fGetPatrolPosition()
+    private void GetPatrolPosition()
     {
         Vector2 vCur;
         do
