c517cee725d1ea5a808ba8e308eca442961dc3d9
HAOXIONG HU <huhaox@deakin.edu.au>
2021-02-01 20:48:37 +0800

    New test scene(New function)
    Redesign the animation controller, rewrite the movement and hit reaction script.
    -WASD: Move
    -Space: Jump
    -HoldShift: Run
    -LeftCtrl: Roll
    -LeftClick: Attack

---
+++ SIT378-NewAnimation/Assets/AnimyController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimyController : MonoBehaviour
+{
+    Animator animator;
+    Collider ObjectCollider;
+    private Collider[] colliders;
+        private Rigidbody[] rigidbodys;
+    public float Health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        ObjectCollider = GetComponent<CapsuleCollider>();
+
+        setRigidbodyState(false);
+        setColliderState(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Bat")
+        {
+            Health -= 50;
+            Debug.Log("You hurt me!");
+            if (Health > 0)
+            {
+                animator.SetTrigger("HitReaction");
+            }
+        }
+    }
+
+    void die()
+    {
+
+    }
+
+    void setRigidbodyState(bool state)
+    {
+        Rigidbody [] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+       // GetComponent<Rigidbody>().isKinematic = !state;
+
+    }
+
+    void setColliderState(bool state)
+    {
+        Collider [] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+
+        GetComponent<Collider>().enabled = !state;
+        GetComponent<CapsuleCollider>().enabled = !state;
+    }
+
+}
---
+++ SIT378-NewAnimation/Assets/Models/DmageColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageColorChange : MonoBehaviour
+{
+    public Color randomlySelectedColor;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        randomlySelectedColor = GetRandomColor();
+        GetComponent<Renderer>().material.color = randomlySelectedColor;
+
+        if (collider.gameObject.tag == "Bat")
+        {
+            Debug.Log("You hurt me!");
+        }
+
+    }
+    private Color GetRandomColor()
+    {
+        return new Color(
+            r: UnityEngine.Random.Range(0f, 1f),
+            g: UnityEngine.Random.Range(0f, 1f),
+            b: UnityEngine.Random.Range(0f, 1f));
+
+    }
+}
